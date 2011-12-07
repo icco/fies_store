24e6b4ab4e2e98bc1fe4adfba32befc7b103c924
Nat Welch <nat@natwelch.com>
2011-12-06 19:56:03 -0800

    Starting to hash out a data model.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,28 @@
 // @author Nat Welch (nat@natwelch.com)
 
+// Basic Inventory Unit
+var Item = Class.create({
+  id: null,
+  type: "",
+  description: "",
+  creator: "",
+  suggested_price: 0.0,
+  sale_price: 0.0,
+
+  init: function() {
+    console.log('You instantiated a Class!');
+  },
+  save: function() {
+    // TODO: Write save method to write to localStorage
+  },
+});
+
+var Commission = Class.create({
+  // TODO(icco): Figure out how Commissions work. What's the workflow?
+  id: null,
+});
+
+// Man document thread. Runs at page load.
 $(document).ready(function() {
 
   // Tests!
@@ -10,6 +33,4 @@ $(document).ready(function() {
   // For storage, this is basics:
   localStorage["bar"] = "foo";
   console.log(localStorage["bar"]);
-
-
 });
