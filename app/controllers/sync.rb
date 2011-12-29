16fb51e41de02f0ac5aca9379beff65ff3b8e612
Nat Welch <nat@natwelch.com>
2011-12-29 12:30:52 -0800

    Base for pushing and pulling.

---
+++ app/controllers/sync.rb
@@ -0,0 +1,16 @@
+# Server endpoints to deal with syncronizing server and client.
+# All routes are under /sync
+
+FiesStore.controllers :sync do
+  # We send in a json serialized dump of localStorage. Turn into database and store.
+  post :push do
+
+  end
+
+
+  # Dump db to client.
+  get :pull do
+
+    "PULL"
+  end
+end
