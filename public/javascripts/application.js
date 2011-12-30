9166f64915613f6a88de1c9b63f01c61fba132d1
Nat Welch <nat@natwelch.com>
2011-12-30 13:19:41 -0800

    Inline editing and page state now work.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,4 @@
 // @author Nat Welch (nat@natwelch.com)
-var DB_NAME = "FiesDatabase";
-var DB_DESCRIPTION = "Item and Commission data for the Fies Store.";
-var DB_VERSION = 1;
 
 // Basic Inventory Unit
 function Item() {
@@ -16,10 +13,38 @@ function Item() {
     console.log('You instantiated a Class!');
   };
 
+
   this.save = function() {
+    if (this.id == null)
+      this.id = "item_" + localStorage.length;
+
     localStorage[this.id] = JSON.stringify(this);
+
+    console.log("Saved: " + this.id);
   };
 }
+
+// Static function for Item loading.
+Item.load = function(id) {
+
+  // TODO(icco): Add checking to make sure this is a valid key.
+  var str = localStorage[id];
+  var obj = JSON.parse(str);
+
+  var i = new Item();
+
+  i.id = id;
+  i.type = obj.type;
+  i.description = obj.description;
+  i.creator = obj.creator;
+  i.suggested_price = obj.suggested_price;
+  i.sale_price = obj.sale_price;
+
+
+  console.log("Loaded: " + i.id);
+
+  return i;
+};
 
 function Commission() {
   // TODO(icco): Figure out how Commissions work. What's the workflow?
@@ -30,21 +55,17 @@ function Commission() {
 // Pull in data from localStorage
 function loadLocalStorageData(datatable) {
   for (var i = 0; i < localStorage.length; i++) {
-    var str = localStorage[localStorage.key(i)];
 
-    // TODO(icco): Add checking to make sure this is a valid key.
-    var obj = JSON.parse(str);
+    var obj = Item.load(localStorage.key(i))
 
     datatable.fnAddData([
       obj.id,
       obj.description,
       obj.type,
       obj.creator,
-      '$' + obj.suggested_price.toFixed(2),
-      '$' + obj.sale_price.toFixed(2),
+      obj.suggested_price.toFixed(2),
+      obj.sale_price.toFixed(2),
     ]);
-
-    console.log("Loaded: " + obj.id);
   }
 }
 
@@ -57,10 +78,10 @@ document.db = {
     console.log(el);
   },
 
-  updateRow: function (el, arr) {
+  push: function () {
   },
 
-  sync: function () {
+  pull: function() {
   }
 };
 
@@ -107,13 +128,13 @@ $(document).ready(function() {
     $('td', $('#items').dataTable().fnGetNodes()).editable(function(value, settings) {
       var id = $(this).parent().children().html();
 
-      var item = new Item();
-      item.id = id;
-      item.description = 'desc';
-      item.type = 'b';
-      item.creator = 'a';
-      item.suggested_price = 0;
-      item.sale_price = 0;
+      var item = Item.load(id);
+
+      item.description = $(this).parent().children()[1].innerText == "" ? value : item.description;
+      item.type = $(this).parent().children()[2].innerText == "" ? value : item.type;
+      item.creator = $(this).parent().children()[3].innerText == "" ? value : item.creator;
+      item.suggested_price = $(this).parent().children()[4].innerText == "" ? parseFloat(value) : item.suggested_price;
+      item.sale_price = $(this).parent().children()[5].innerText == "" ? parseFloat(value) : item.sale_price;
       item.save();
 
       // We must return what we want to display.
